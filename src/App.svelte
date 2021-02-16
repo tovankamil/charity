41007ea9533f5fa53c481f295cd4c03b1e4f3962
tovan kamil <tovan.kamil@gmail.com>
2021-02-16 10:19:42 +0700

    style div global

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,25 @@
 <script>
-	import CharityList from "./CharityList.svelte";
 
-	let title="Charity";
-	setTimeout(function(){
-		title='indonesia';
-	},5000)
 </script>
 
-<div>
-	<h1>{title}</h1>
-	<CharityList/>
-</div>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+</svelte:head>
+<main class="flex-shrink-0">
+	<div class="container">
+	<h1>Hai sahabat  LIbren G</h1>
+	</div>
+  </main>
+<footer class="footer mt-auto py-3 bg-light">
+	<div class="container">
+	  <span class="text-muted">Place sticky footer content here.</span>
+	</div>
+  </footer>
+
 
 <style>
-	h1{
-		font-size: 3rem;
-		color:red
-	}
-	div{
-		border:red 1px solid
-	}
+
 </style>
--- src/CharityList.svelte
+++ src/CharityList.svelte
@@ -1,14 +1,1 @@
-<div>
-    <h2>Daftar Charity</h2>
-    <ul>
-        <li>
-            Sumbangan masjid
-        </li>
-        <li>
-            Sumbangan masjid
-        </li>
-        <li>
-            Sumbangan masjid
-        </li>
-    </ul>
-</div>
+
