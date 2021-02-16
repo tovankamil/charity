42f5ca5e50a5b58fed688511268f9b4a298c6965
tovan kamil <tovan.kamil@gmail.com>
2021-02-16 11:00:49 +0700

    LIBREN

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,49 @@
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" >
 
 </svelte:head>
 <main class="flex-shrink-0">
 	<div class="container">
-	<h1>Hai sahabat  LIbren G</h1>
+	<h1 class="text-center pt-5 second-color primary-font">Hai sahabat  <span class="dt primary-color bold-700">LIBREN G</span> </h1>
+	<p class="text-center pt-5 primary-color ">Kali ini  ada beberapa hal penting yang harus diperhatikan dalam kehidupan kita !</p>
+	<p class="text-center pt-4  primary-font">Apakah anda mengalami <span  class="second-color bold-700 ">depresi?</span> </p>
+	<p class="text-center   primary-font">Apakah anda mengalami <span class=" second-color bold-700">insomia?</span> </p>
+	<p class="text-center   primary-font">Apakah anda mengalami <span class=" second-color bold-700">gelisah?</span> </p>
+	<p class="text-center  primary-font">Apakah anda atau buah hati  sangat untuk sulit <span class=" second-color bold-700">mengingat suatu hal penting ?</span> </p>
+	<p class="text-center  primary-font">Apakah anda atau buah hati  sangat untuk sulit <span class=" second-color bold-700">sulit menerima banyak informasi 
+		dalam <span class=" second-color bold-700">pembelajaran  anak?</span>
+	</span> </p>
+
+	<p  class="text-center pt-5 second-color primary-font">Hati-hari hal itu dapat disebabkan karena <span class=" primary-color bold-700">STRESS</span>  pada diri anda atau anak anda</p>
+	<h1 class="text-center pt-5 primary-color primary-font bold-900">INGAT ! JANGAN ANGGAP ENTENG DAMPAK BURUK DARI STRESS ANDA</h1>
+
+	<p class="primary-font text-center pt-4">
+	<span class="primary-color bold-900 lineHeight-18">Stress</span>	  tidak hanya berdampak negatif pada kondisi psikologis atau kesehatan mental, 
+tapi juga dapat merusak otak penderitanya. 
+Sejumlah penelitian kini menemukan bahwa orang
+ yang mengalami depresi rentan mengalami penuaan
+ dini pada otak serta gangguan fungsi otak
+	</p>
+
+	<p class="primary-font text-center pt-4 lineHeight-18"><span class="primary-color bold-900">Otak manusia </span>adalah struktur pusat pengaturan yang memiliki volume sekitar 1.350 cc dan terdiri lebih dari 100 juta sel saraf atau neuron. Otak mengatur dan mengkordinir sebagian besar, gerakan, perilaku dan fungsi tubuh homeostasis seperti detak jantung, tekanan darah, keseimbangan cairan tubuh dan suhu tubuh.
+	</p>
+	<p class="primary-font text-center pt-4 lineHeight-18">
+		<span class="primary-color bold-900">Otak manusia</span> bertanggung jawab terhadap pengaturan seluruh badan dan pemikiran manusia. dan juga bertanggung jawab atas fungsi seperti pengenalan, emosi. ingatan, pembelajaran motorik dan segala bentuk pembelajaran lainnya.
+
+	</p>
+
+	<p class="primary-font text-center pt-4 lineHeight-18">
+		Berkaitan dengan fakta vitalnya fungsi otak tersebut, maka tidaklah berlebihan jika dikatakan bahwasanya sebagian besar penyakit diakibatkan oleh <span class="second-color bold-900">kinerja otak yang berkurang atau terganggu</span>. Sehingga anda tidak mudah lupa
+	</p>
+
+	<h2 class=" text-center pt-4 ">ANDA TIDAK PERLU KHAWATIR
+		DENGAN SEMUA ITU </h2>
+		<h1 class="primary-font text-center pt-4 primary-color bold-900">LIBREN G</h1>
+		<p class="primary-font text-center font-11"><span class="primary-color bold-900">Nutrisi herbal alami</span>  yang di ekstrak dari daun Pegagan yang diyakini mampu secara cepat melancarkan sirkulasi darah ke otak manusia.</p>
+		<img class="img-fluid" src="images/LibrenG.jpg" alt="">
 	</div>
   </main>
 <footer class="footer mt-auto py-3 bg-light">
